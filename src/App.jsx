b39a307f1090fdc3492e1d59149dd6b2d8c54c60
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-10-22 10:24:02 +0530

    Learnt link and navigate hook with website program

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
 import './App.css'
-import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
+import {BrowserRouter,Routes,Route,Link,useNavigate} from "react-router-dom"
 
-function App() {
- 
-  //link used to directly fetch without the loader
 
+function App(){
+ //link used to directly fetch without the loader
   return <div>
- <BrowserRouter>
-   
+  <BrowserRouter>
+    <Link to="/">Allen</Link>
 
-  <Routes>
-    <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-    <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-    <Route path="/" element={<Landing />}/>
-  </Routes>
- </BrowserRouter>
+    <Link to="/neet/online-coaching-class-11">Class11</Link>
+
+    <Link to="/neet/online-coaching-class-12">Class12</Link> 
+
+    <Routes>
+      <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+      <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+      <Route path="/" element={<Landing />}/>
+    </Routes>
+  </BrowserRouter>
  </div>
 }
+
+
 function Class11Program(){
   return <div>
     NEET Program for the class 11th students
   </div>
 }
 
-function Class12Program(){
-  return <div>
+
+function Class12Program()
+{
+ const navigate=useNavigate();
+
+ function redirectUser()
+ {
+  navigate("/")
+ }
+
+
+ return <div>
     NEET Program for the class 12th students
+    <button onClick={redirectUser}>go back to landing page</button>
   </div>
 }
+
 
 function Landing()
 {
@@ -35,5 +52,4 @@ function Landing()
     welcome to allen
   </div>
 }
-
 export default App
