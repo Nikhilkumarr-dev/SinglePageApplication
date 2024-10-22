98dbe4156e3f52ce045e15882eea1a4128f51c2e
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-10-22 10:50:48 +0530

    learned layout hook with the help of outlet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import './App.css'
-import {BrowserRouter,Routes,Route,Link,useNavigate} from "react-router-dom"
+import {BrowserRouter,Routes,Route,Link,useNavigate, Outlet} from "react-router-dom"
 
 
 function App(){
  //link used to directly fetch without the loader
+ //only the route has the strict has neet route 
   return <div>
   <BrowserRouter>
+    <Routes>
+      <Route path="/neet" element={<Layout />}>
+        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+        <Route path="/" element={<Landing />}/>
+        <Route path="*" element={<ErrorPage />}/>
+      </Route>  
+    </Routes>
+  </BrowserRouter>
+  Contact us later
+ </div>
+}
+
+//this layout help us to render main route and then render the child routes
+function Layout()
+{
+  return <div style={{height:"100vh"}}>
     <Link to="/">Allen</Link>
 
     <Link to="/neet/online-coaching-class-11">Class11</Link>
 
     <Link to="/neet/online-coaching-class-12">Class12</Link> 
-
-    <Routes>
-      <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-      <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-      <Route path="/" element={<Landing />}/>
-      <Route path="*" element={<ErrorPage />}/>
-    </Routes>
-  </BrowserRouter>
- </div>
+      <div style={{height:"90vh"}}>
+        <Outlet/>
+      </div>
+    footer
+  </div>
 }
-
+//this outlet renders child routes after rendering the parent route over the application
 
 function Class11Program(){
   return <div>
