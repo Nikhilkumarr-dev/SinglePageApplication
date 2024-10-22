4ba89cbddb84b1957c5f26ff9f71b26b7cf57d1f
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-10-22 11:26:38 +0530

    We use the useRef for the stopClock

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
+import { useState ,useRef} from 'react'
 import './App.css'
 
 //a clock with start and stop button
 function App(){ 
   const [currentCount,setCurrentCount]=useState(1);
-  const [timer,setTimer]=useState(0);
-  //this take rendering to update the value
+  const timer = useRef();
 
   function startClock()
   {
     let value = setInterval(function(){
       setCurrentCount(c=>c+1);
     },1000);
-    setTimer(value)
+    timer.current=value;
+   
   }
+   //here the rerender doesnt take
 
   function stopClock()
   {
-    clearInterval(timer);
+    clearInterval(timer.current);
   }
 
   return <div>
