d54acf56d98fabd6c54844e01a38edec92504ee4
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-10-22 11:21:27 +0530

    2nd case of useref example using usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react'
 import './App.css'
-import { useRef } from 'react'
+
+//a clock with start and stop button
 function App(){ 
-  const inputref = useRef();
-  function focusOnInput()
+  const [currentCount,setCurrentCount]=useState(1);
+  const [timer,setTimer]=useState(0);
+  //this take rendering to update the value
+
+  function startClock()
   {
-    //document.getElementbyId("name");
-    inputref.current.focus();
+    let value = setInterval(function(){
+      setCurrentCount(c=>c+1);
+    },1000);
+    setTimer(value)
   }
+
+  function stopClock()
+  {
+    clearInterval(timer);
+  }
+
   return <div>
-    SignUp
-    <input ref={inputref} type={"text"}></input>
-    <input type={"text"}></input>
-    <button onClick={focusOnInput}>submit</button>
- </div>
+    {currentCount}
+    <br/>
+    <button onClick={startClock}>Start</button>
+    <button onClick={stopClock}>stop</button>
+  </div>
+
 }
 
 export default App
